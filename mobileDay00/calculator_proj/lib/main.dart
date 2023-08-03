577b8611522330e5c931e271521219905fca4737
Marcos Chiu <marcos.chiu@gmail.com>
2023-08-03 16:31:17 -0300

    feat: MVP refactored

--- mobileDay00/calculator_proj/lib/main.dart
+++ mobileDay00/calculator_proj/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:math_expressions/math_expressions.dart';
 import 'buttons.dart';
 import 'text.dart';
+import 'utils.dart';
 
 void main() {
   runApp(const MyApp());
@@ -140,90 +140,5 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
-  }
-}
-
-double min(double a, double b) {
-  if (a < b) {
-    return (a);
-  } else {
-    return (b);
-  }
-}
-
-String lastCharacter(String text) {
-  return (text.split('').last);
-}
-
-bool isCharacterOperator(String text) {
-  String lastChar = lastCharacter(text);
-  List<String> operators = ['+', '-', '*', '/'];
-
-  return (operators.contains(lastChar));
-}
-
-bool containsDecimalInLastNumber(String text) {
-  List<String> splitText = text.split(RegExp(r'[-+*/]'));
-
-  return (splitText.last.contains('.'));
-}
-
-String processInput(String text, String value) {
-  if (text.length > 15) return (text);
-
-  if (int.tryParse(value) != null) {
-    if (text == '0') {
-      return (value);
-    } else {
-      return (text += value);
-    }
-  }
-  if (value == '.') {
-    if (lastCharacter(text) != '.' &&
-        !containsDecimalInLastNumber(text) &&
-        (int.tryParse(lastCharacter(text)) != null)) {
-      return (text += value);
-    }
-  }
-  if (isCharacterOperator(value)) {
-    if (!isCharacterOperator(lastCharacter(text)) &&
-        lastCharacter(text) != '.') {
-      return (text += value);
-    } else if (value == '-' &&
-        lastCharacter(text) != '-' &&
-        lastCharacter(text) != '.') {
-      return (text += value);
-    }
-  }
-  return (text);
-}
-
-String processOutput(text) {
-  if (int.tryParse(lastCharacter(text)) == null) {
-    return ("Uncomplete expression");
-  }
-
-  Parser p = Parser();
-  Expression exp = p.parse(text);
-
-  ContextModel cm = ContextModel();
-  try {
-    double result = exp.evaluate(EvaluationType.REAL, cm);
-
-    if (result.isInfinite) {
-      return ("Can't divide by zero");
-    }
-
-    String output = result.truncateToDouble() == result
-        ? result.truncate().toString()
-        : result.toString();
-
-    return (output);
-  } catch (e) {
-    if (e is Exception) {
-      return ("Error: Invalid expression");
-    } else {
-      return ("Error: ${e.toString()}");
-    }
   }
 }
---
+++ mobileDay00/calculator_proj/lib/utils.dart
@@ -0,0 +1,86 @@
+import 'package:math_expressions/math_expressions.dart';
+
+double min(double a, double b) {
+  if (a < b) {
+    return (a);
+  } else {
+    return (b);
+  }
+}
+
+String lastCharacter(String text) {
+  return (text.split('').last);
+}
+
+bool isCharacterOperator(String text) {
+  String lastChar = lastCharacter(text);
+  List<String> operators = ['+', '-', '*', '/'];
+
+  return (operators.contains(lastChar));
+}
+
+bool containsDecimalInLastNumber(String text) {
+  List<String> splitText = text.split(RegExp(r'[-+*/]'));
+
+  return (splitText.last.contains('.'));
+}
+
+String processInput(String text, String value) {
+  if (text.length > 15) return (text);
+
+  if (int.tryParse(value) != null) {
+    if (text == '0') {
+      return (value);
+    } else {
+      return (text += value);
+    }
+  }
+  if (value == '.') {
+    if (lastCharacter(text) != '.' &&
+        !containsDecimalInLastNumber(text) &&
+        (int.tryParse(lastCharacter(text)) != null)) {
+      return (text += value);
+    }
+  }
+  if (isCharacterOperator(value)) {
+    if (!isCharacterOperator(lastCharacter(text)) &&
+        lastCharacter(text) != '.') {
+      return (text += value);
+    } else if (value == '-' &&
+        lastCharacter(text) != '-' &&
+        lastCharacter(text) != '.') {
+      return (text += value);
+    }
+  }
+  return (text);
+}
+
+String processOutput(text) {
+  if (int.tryParse(lastCharacter(text)) == null) {
+    return ("Complete the expression");
+  }
+
+  Parser p = Parser();
+  Expression exp = p.parse(text);
+
+  ContextModel cm = ContextModel();
+  try {
+    double result = exp.evaluate(EvaluationType.REAL, cm);
+
+    if (result.isInfinite) {
+      return ("Can't divide by zero");
+    }
+
+    String output = result.truncateToDouble() == result
+        ? result.truncate().toString()
+        : result.toString();
+
+    return (output);
+  } catch (e) {
+    if (e is Exception) {
+      return ("Error: Invalid expression");
+    } else {
+      return ("Error: ${e.toString()}");
+    }
+  }
+}
