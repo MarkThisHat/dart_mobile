469970f90ad614df3eab14f39f6718ffb4ec8906
Marcos <marcos.chiu@gmail.com>
2023-08-05 21:18:21 -0300

    style: refactored topbar widget

--- mobileDay01/mobileWeatherApp/ex00/lib/widgets/topbar.dart
+++ mobileDay01/mobileWeatherApp/ex00/lib/widgets/topbar.dart
@@ -15,54 +15,72 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
       titleSpacing: 0.0, // This makes the row use the full width of the AppBar.
       title: Row(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                color: theme.colorScheme.onPrimary,
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: TextField(
-                cursorColor: theme.colorScheme.onPrimary,
-                decoration: InputDecoration(
-                  hintText: "Search location...",
-                  hintStyle: TextStyle(
-                      fontSize: 16.0,
-                      color: theme.colorScheme.onPrimary.withOpacity(0.5)),
-                  filled: false,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(
-                      color: theme.colorScheme.onPrimary.withOpacity(0.5),
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(
-                      color: theme.colorScheme.onPrimary,
-                    ),
-                  ),
-                ),
-                style: TextStyle(
-                    fontSize: 16.0, color: theme.colorScheme.onPrimary),
-              ),
-            ),
-          ),
-          IconButton(
-            icon: Icon(Icons.location_on, color: theme.colorScheme.onPrimary),
-            onPressed: () {
-              // Implement your geolocation functionality here
-            },
-          ),
+          _buildTitle(theme),
+          _buildSearchField(theme),
+          _buildGeoLocationButton(theme),
           const SizedBox(width: 8.0),
         ],
+      ),
+    );
+  }
+
+  Widget _buildTitle(ThemeData theme) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          color: theme.colorScheme.onPrimary,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGeoLocationButton(ThemeData theme) {
+    return IconButton(
+      icon: Icon(Icons.location_on, color: theme.colorScheme.onPrimary),
+      onPressed: () {
+        //TODO: implement geolocation
+      },
+    );
+  }
+
+  Widget _buildSearchField(ThemeData theme) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: _buildSearchTextField(theme),
+      ),
+    );
+  }
+
+  Widget _buildSearchTextField(ThemeData theme) {
+    return TextField(
+      cursorColor: theme.colorScheme.onPrimary,
+      decoration: _buildTextFieldDecoration(theme),
+      style: TextStyle(fontSize: 16.0, color: theme.colorScheme.onPrimary),
+    );
+  }
+
+  InputDecoration _buildTextFieldDecoration(ThemeData theme) {
+    return InputDecoration(
+      hintText: "Search location...",
+      hintStyle: TextStyle(
+          fontSize: 16.0, color: theme.colorScheme.onPrimary.withOpacity(0.5)),
+      filled: false,
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(
+          color: theme.colorScheme.onPrimary.withOpacity(0.5),
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(
+          color: theme.colorScheme.onPrimary,
+        ),
       ),
     );
   }
