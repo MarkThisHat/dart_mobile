10a0bb3842cb0980d36a7433e0fc9da4e92b4313
Marcos <marcos.chiu@gmail.com>
2023-08-06 00:31:34 -0300

    feat: MVP ex01

---
+++ mobileDay01/mobileWeatherApp/weather/lib/widgets/body.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class BodyTabBarView extends StatelessWidget {
+  final TabController tabController;
+  final String displayText;
+
+  const BodyTabBarView(
+      {super.key, required this.tabController, required this.displayText});
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    TextStyle textStyle =
+        TextStyle(fontSize: 28.0, color: theme.colorScheme.onBackground);
+
+    return Container(
+      color: theme.colorScheme.background,
+      child: TabBarView(
+        controller: tabController,
+        children: [
+          _buildTabContent('Currently', displayText, textStyle),
+          _buildTabContent('Today', displayText, textStyle),
+          _buildTabContent('Weekly', displayText, textStyle),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTabContent(String label, String displayText, TextStyle style) {
+    return Center(
+      child: Text(
+        '$label\n$displayText',
+        style: style,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
